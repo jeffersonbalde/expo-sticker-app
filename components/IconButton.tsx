58fa19d8460a7f6ab8483f02ede25c3ffec88d52
Jefferson Balde <jefferson.balde@sccpag.edu.ph>
2024-08-26 13:06:32 +0800

    modal created

---
+++ components/IconButton.tsx
@@ -0,0 +1,22 @@
+import { Text, StyleSheet, Pressable } from "react-native"
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+export default function IconButton({ icon, label, onPress }) {
+    return (
+        <Pressable style={styles.iconButton} onPress={onPress}>
+            <MaterialIcons name={icon} size={24} color="#000000" />
+            <Text style={styles.iconButtonLabel}>{label}</Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    iconButton: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    iconButtonLabel: {
+      color: '#000000',
+      marginTop: 12,
+    },
+  });
